2d1a34e36f2c19b7966397b730b4f154c72d80e0
ardhiesta <ardhi.mail@gmail.com>
2017-12-07 09:27:22 +0700

    fix @PATH parameter

--- app/src/main/java/id/web/ardhi/mahasiswaapp/service/MahasiswaService.java
+++ app/src/main/java/id/web/ardhi/mahasiswaapp/service/MahasiswaService.java
@@ -11,25 +11,26 @@ import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 /**
  * Created by linuxluv on 08/11/17.
  */
 
 public interface MahasiswaService {
-    @GET("all_mahasiswa")
+    @GET("mahasiswa/all")
     Call<List<Mahasiswa>> getAllMhs();
 
     @GET("mahasiswa/{nim}")
-    Call<Mahasiswa> getMhsByNim();
+    Call<Mahasiswa> getMhsByNim(@Path("nim") String nim);
 
     @Headers("Content-Type: application/json")
     @POST("mahasiswa/new")
     Call<ResponseBody> createMhs(@Body Mahasiswa mahasiswa);
 
-    @PUT("mahasiswa/update")
-    Call<Mahasiswa> updateMhs(@Body Mahasiswa mahasiswa);
+    @PUT("mahasiswa/update/{old_nim}")
+    Call<ResponseBody> updateMhs(@Path("old_nim") String oldNim, @Body Mahasiswa mahasiswa);
 
     @DELETE("mahasiswa/{nim}")
-    Call<ResponseBody> deleteMhs();
+    Call<ResponseBody> deleteMhs(@Path("nim") String nim);
 }
